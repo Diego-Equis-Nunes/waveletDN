7c0aec384bfd4cd2aca757b3e413f45813cd0ab8
Diego-Equis-Nunes <diegoequisnunes@gmail.com>
2023-01-18 16:04:13 -0800

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -4,11 +4,11 @@ import java.net.URI;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
+    int num = 7;
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
+            return String.format("Diego Nunes' Number: %d", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
@@ -18,7 +18,7 @@ class Handler implements URLHandler {
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("count")) {
                     num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                    return String.format("Diego Nunes' Number increased by %s! It's now %d", parameters[1], num);
                 }
             }
             return "404 Not Found!";
